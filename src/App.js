433fef7863dada4965a05ca04e11d9a12180f848
Pervez alam <pervezalam777@gmail.com>
2021-12-01 20:31:17 +0530

    some changes to UI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   const areaRef = useRef();
   const [processed, setProcessed] = useState(false);
-  const [person, setPerson] = useState('');
+  //const [person, setPerson] = useState('');
   const [value, setValue] = useState(-1);
 
   const handleClick = () => {
@@ -14,21 +14,21 @@ function App() {
     setProcessed(processed);
   }
 
-  const onChangeValue = (event) => {
-    const method = event.target.value;
-    const result = aiCalculator[method](person);
-    setValue(`${method}: ${result}`)
-  }
+  // const onChangeValue = (event) => {
+  //   const method = event.target.value;
+  //   const result = aiCalculator[method](person);
+  //   setValue(`${method}: ${result}`)
+  // }
 
   const handleTotalHouseExpense = () => {
     const result = aiCalculator.totalHouseExpense()
     setValue(`Total House expense ${result}`);
   }
 
-  const onPersonSelectionChange = (event) => {
-    const name = event.target.value;
-    setPerson(name);
-  }
+  // const onPersonSelectionChange = (event) => {
+  //   const name = event.target.value;
+  //   setPerson(name);
+  // }
 
   function renderOptions() {
     if(processed) {
@@ -37,38 +37,65 @@ function App() {
           <div>
             <button onClick={handleTotalHouseExpense}> totalHouseExpense </button>
           </div>
-          <div>
-            <h4>Select name </h4>
-            <ul onChange={onPersonSelectionChange}>
-              {
-                aiCalculator.getPersonList().map(name => (
-                  <li key={name}>
-                    <input type="radio" value={name} name="person" /> {name}
-                  </li>
-                ))
-              }
-            </ul>
-          </div>
-
-          <div onChange={onChangeValue}>
-            <h4> Select option </h4>
-            <div>
-              <input type="radio" value="paidFromOwnPocket" name="option" /> Paid From Own Pocket
-            </div>
-            <div>
-              <input type="radio" value="paidBy" name="option" /> Paid By
-            </div>
-            <div>
-              <input type="radio" value="receivedBy" name="option" /> Received By
-            </div>
-          </div>
-      
           <div>Result: {value}</div>
+          {renderPersonExpenseTable()}
           {renderTable()}
         </>
       )
     }
     return null;
+  }
+
+  function renderPersonExpenseTable() {
+    return (
+      <>
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Paid</th>
+            <th>Received</th>
+            <th>Paid from own pocket</th>
+          </thead>
+          <tbody>
+          {
+              aiCalculator.getPersonList().map(name => (
+                <tr key={name}>
+                  <td>{name}</td>
+                  <td>{aiCalculator.paidBy(name)}</td>
+                  <td>{aiCalculator.receivedBy(name)}</td>
+                  <td>{aiCalculator.paidFromOwnPocket(name)}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        {/* <div>
+          <h4>Select name </h4>
+          <ul onChange={onPersonSelectionChange}>
+            {
+              aiCalculator.getPersonList().map(name => (
+                <li key={name}>
+                  <input type="radio" value={name} name="person" /> {name}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div onChange={onChangeValue}>
+          <h4> Select option </h4>
+          <div>
+            <input type="radio" value="paidFromOwnPocket" name="option" /> Paid From Own Pocket
+          </div>
+          <div>
+            <input type="radio" value="paidBy" name="option" /> Paid By
+          </div>
+          <div>
+            <input type="radio" value="receivedBy" name="option" /> Received By
+          </div>
+        </div> */}
+      </>
+     
+    )
   }
 
   function renderTable() {
